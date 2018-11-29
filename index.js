ad2fa704aaca628a40088665bfbb477ebe78851e
torsina <theo.sagot.1@gmail.com>
2018-11-30 00:43:33 +0100

    create class

---
+++ index.js
@@ -0,0 +1,30 @@
+// Libraries
+const path = require("path");
+const fs = require("fs");
+const grpc = require("grpc");
+const protoLoader = require("@grpc/proto-loader");
+const grpcUrl = require("@arys/grpc-url");
+// consts
+const grpcServers = JSON.parse(fs.readFileSync("./config/grpc.json"));
+
+class grpcClient {
+    constructor() {
+        this.proto = {};
+        this.clients = {};
+        this._proto = {};
+        Object.keys(grpcServers).forEach((server) => {
+            // load protofile
+            this._proto[server] = {};
+            this._proto[server].path = path.join(__dirname,
+                `../../node_modules/@arys/protofiles/src/${server}.proto`);
+            this._proto[server].definition = protoLoader.loadSync(this._proto[server].path);
+            this._proto[server].object = grpc.loadPackageDefinition(this._proto[server].definition);
+            this.proto[grpcServers[server].name] = this._proto[server].object[server];
+            // start grpc client
+            this.clients[grpcServers[server].name] = new this.proto[grpcServers[server].name](grpcUrl(grpcServers[server].serviceName.toUpperCase().replace("-", "_")), // eslint-disable-line
+                grpc.credentials.createInsecure(), grpcServers[server].config);
+        });
+    }
+}
+
+module.exports = grpcClient;
